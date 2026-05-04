79f60421902082f00bce8bc3b82e43af84ac6b77
lorranelias514-lab <lorranelias514@gmail.com>
2026-05-04 09:33:18 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--seletor de caracteres--
+letra = input("Digite a letra")
+if letra == "A":
+  print("Você escolhe a letra A")
+elif letra == "B":
+  print("Você escolheu a letra "B")
+else:
+  print("Essa opção não está disponível")
