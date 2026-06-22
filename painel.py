14a2f87aeaaf269f39d1ab0ae414bb0c094c7388
lorranelias514-lab <lorranelias514@gmail.com>
2026-06-22 12:29:11 +0000

    v1

---
+++ painel.py
@@ -0,0 +1,6 @@
+print("--- PAINEL DE ATENDIMENTO ---")
+fila_espera = ["senha 01", "senha 02", "senha 03" , "senha 04"]
+print(f"O próximo paciente a ser atendido é: {fila_espera [0]}")
+print(f"O segundo paciente da fila é: {fila_espera [1]}")
+total = len(fila_espera)
+print(f"total de pacientes na fila: {total}")
